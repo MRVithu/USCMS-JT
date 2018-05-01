a26cc07afe1eee1f02ad8752149d2e043e9d1a38
Vithu <mrvithu@hotmail.com>
2018-05-01 16:41:13 +0530

    24th commit.

--- src/main/java/com/vithu/uscms/controller/product/BrandManagementController.java
+++ src/main/java/com/vithu/uscms/controller/product/BrandManagementController.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
--- src/main/java/com/vithu/uscms/controller/user/CustomerManagementController.java
+++ src/main/java/com/vithu/uscms/controller/user/CustomerManagementController.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.json.JSONException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
